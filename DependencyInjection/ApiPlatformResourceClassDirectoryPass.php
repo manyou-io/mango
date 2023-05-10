3fb5469b28429c187b3cb4837d8e1cb804a58ce0
root <brent@manyou.io>
2023-05-10 04:20:31 +0000

    Add ApiPlatformResourceClassDirectoryPass

---
+++ DependencyInjection/ApiPlatformResourceClassDirectoryPass.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Manyou\Mango\DependencyInjection;
+
+use Symfony\Component\Config\Resource\DirectoryResource;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ApiPlatformResourceClassDirectoryPass implements CompilerPassInterface
+{
+    public function __construct(
+        private array $paths = [],
+    ) {
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        if ($this->paths === []) {
+            return;
+        }
+
+        $resourceDirs = $container->hasParameter('api_platform.resource_class_directories')
+            ? $container->getParameter('api_platform.resource_class_directories')
+            : [];
+
+        foreach ($this->paths as $path) {
+            $resourceDirs[] = $path;
+            $container->addResource(new DirectoryResource($path, '/\.(xml|ya?ml|php)$/'));
+        }
+
+        $container->setParameter('api_platform.resource_class_directories', $resourceDirs);
+    }
+}
