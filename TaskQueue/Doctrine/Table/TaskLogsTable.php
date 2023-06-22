8210d715586c576c49d57247332426ef055b95a4
root <brent@manyou.io>
2023-06-22 07:48:35 +0000

    Use ObjectJsonType

--- TaskQueue/Doctrine/Table/TaskLogsTable.php
+++ TaskQueue/Doctrine/Table/TaskLogsTable.php
@@ -9,6 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Manyou\Mango\Doctrine\Contract\TableProvider;
 use Manyou\Mango\Doctrine\Table;
 use Manyou\Mango\Doctrine\Type\LogLevelType;
+use Manyou\Mango\Doctrine\Type\ObjectJsonType;
 
 class TaskLogsTable implements TableProvider
 {
@@ -21,8 +22,8 @@ class TaskLogsTable implements TableProvider
         $table->addColumn('task_id', 'ulid');
         $table->addColumn('level', LogLevelType::NAME);
         $table->addColumn('message', Types::TEXT);
-        $table->addColumn('context', Types::JSON);
-        $table->addColumn('extra', Types::JSON);
+        $table->addColumn('context', ObjectJsonType::NAME);
+        $table->addColumn('extra', ObjectJsonType::NAME);
         $table->setPrimaryKey(['id']);
         $table->addForeignKeyConstraint(TasksTable::NAME, ['task_id'], ['id']);
 
