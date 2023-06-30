64bb4ea67dbbdb14f81062b9fcd85c6241a6f9b5
root <brent@manyou.io>
2023-06-30 04:12:44 +0000

    Fix CachedJWKSLoader cache key

--- Jose/CachedJWKSLoader.php
+++ Jose/CachedJWKSLoader.php
@@ -25,7 +25,7 @@ class CachedJWKSLoader implements JWKSLoader
 
     public function __invoke(): JWKSet
     {
-        return $this->cache->get($this->url, function (ItemInterface $item) {
+        return $this->cache->get(spl_object_hash($this), function (ItemInterface $item) {
             $item->expiresAfter($this->expiresAfter);
 
             return $this->jkuFactory->loadFromUrl($this->url, $this->header);
