23ec70171364def4c7edd9e9a04067ba26625782
root <brent@manyou.io>
2023-07-10 00:43:10 +0000

    Update

---
+++ Jose/AlgHeaderChecker.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Manyou\Mango\Jose;
+
+use Jose\Component\Checker\HeaderChecker;
+use Jose\Component\Checker\InvalidHeaderException;
+
+use function is_string;
+use function sprintf;
+
+final class AlgHeaderChecker implements HeaderChecker
+{
+    public function __construct(private readonly string $algorithm)
+    {
+    }
+
+    public function checkHeader(mixed $value): void
+    {
+        if (! is_string($value)) {
+            throw new InvalidHeaderException('The value of the header "alg" is not valid', 'alg', $value);
+        }
+
+        if ($this->algorithm !== $value) {
+            throw new InvalidHeaderException(sprintf('The algorithm "%s" is not known.', $value), 'alg', $value);
+        }
+    }
+
+    public function supportedHeader(): string
+    {
+        return 'alg';
+    }
+
+    public function protectedHeaderOnly(): bool
+    {
+        return true;
+    }
+}
---
+++ Jose/JWTHandler.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Manyou\Mango\Jose;
+
+use Jose\Component\Checker\ClaimCheckerManager;
+use Jose\Component\Checker\HeaderCheckerManager;
+use Jose\Component\Checker\InvalidClaimException;
+use Jose\Component\Checker\InvalidHeaderException;
+use Jose\Component\Core\Util\JsonConverter;
+use Jose\Component\Signature\JWSVerifier;
+use Jose\Component\Signature\Serializer\CompactSerializer as JWSCompactSerializer;
+use LogicException;
+use Psr\Log\LoggerInterface;
+use RuntimeException;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Throwable;
+
+use function is_string;
+
+class JWTHandler implements AccessTokenHandlerInterface
+{
+    public function __construct(
+        private JWKSLoader $jwksLoader,
+        #[Autowire(service: 'jose.header_checker.access_token')]
+        private HeaderCheckerManager $signatureHeaderCheckerManager,
+        #[Autowire(service: 'jose.claim_checker.access_token')]
+        private ClaimCheckerManager $claimCheckerManager,
+        #[Autowire(service: 'jose.jws_verifier.access_token')]
+        private JWSVerifier $jwsLoader,
+        private array $mandatoryClaims = [],
+        private ?LoggerInterface $logger = null,
+        private string $userIdClaim = 'sub',
+    ) {
+    }
+
+    public function getUserBadgeFrom(string $accessToken): UserBadge
+    {
+        try {
+            $claims = $this->verify($accessToken);
+        } catch (InvalidClaimException $e) {
+            if ($e->getClaim() === 'exp') {
+                $this->logger?->debug('The JWT token is expired.');
+            } else {
+                $this->logger?->debug('Invalid JWT Token. The following claim was not verified: "{claim}".', [
+                    'claim' => $e->getClaim(),
+                    'exception' => $e,
+                ]);
+            }
+
+            throw new BadCredentialsException('Invalid credentials.', $e->getCode(), $e);
+        } catch (InvalidHeaderException $e) {
+            $this->logger?->debug('Invalid JWT Token. The following header was not verified: "{header}".', [
+                'header' => $e->getHeader(),
+                'exception' => $e,
+            ]);
+
+            throw new BadCredentialsException('Invalid credentials.', $e->getCode(), $e);
+        } catch (Throwable $e) {
+            $this->logger?->debug('Invalid JWT Token: {message}', [
+                'message' => $e->getMessage(),
+                'exception' => $e,
+            ]);
+
+            throw new BadCredentialsException('Invalid credentials.', $e->getCode(), $e);
+        }
+
+        if (empty($claims[$this->userIdClaim])) {
+            $this->logger?->debug('Invalid JWT Token: User identifier claim "{claim}" not found.', [
+                'claim' => $this->userIdClaim,
+            ]);
+
+            throw new BadCredentialsException('Invalid credentials.');
+        }
+
+        return new UserBadge($claims[$this->userIdClaim], $this->noUserProviderConfigured(...), $claims);
+    }
+
+    private function noUserProviderConfigured(): void
+    {
+        throw new LogicException('You must configure a UserProvider to load user from JWT claims.');
+    }
+
+    private function verify(string $token): array
+    {
+        $serializer = new JWSCompactSerializer();
+
+        $jws = $serializer->unserialize($token);
+        $this->signatureHeaderCheckerManager->check($jws, 0);
+
+        $signatureKeyset = ($this->jwksLoader)();
+        if ($this->jwsLoader->verifyWithKeySet($jws, $signatureKeyset, 0) === false) {
+            throw new RuntimeException('Failed to decode the JWT token.');
+        }
+
+        $jwt = $jws->getPayload();
+        if (! is_string($jwt)) {
+            throw new RuntimeException('Failed to decode the JWT token.');
+        }
+
+        $payload = JsonConverter::decode($jwt);
+        $this->claimCheckerManager->check($payload, $this->mandatoryClaims);
+
+        return $payload;
+    }
+}
