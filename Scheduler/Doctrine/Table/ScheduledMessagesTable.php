7c7d98e09aa9eba9f9008fc97a06ff14c3751b89
root <brent@manyou.io>
2023-07-17 07:58:46 +0000

    Update

--- Scheduler/Doctrine/Table/ScheduledMessagesTable.php
+++ Scheduler/Doctrine/Table/ScheduledMessagesTable.php
@@ -18,7 +18,7 @@ class ScheduledMessagesTable implements TableProvider
     {
         $table = new Table($schema, self::NAME);
         $table->addColumn('key', Types::STRING);
-        $table->addColumn('envelope', Types::JSON, ['jsonb' => true]);
+        $table->addColumn('envelope', Types::JSON)->setPlatformOptions(['jsonb' => true]);
         $table->addColumn('available_at', Types::DATETIME_IMMUTABLE, alias: 'availableAt');
         $table->addColumn('last_dispatched_at', UsDateTimeImmutableType::NAME, alias: 'lastDispatchedAt');
         $table->setPrimaryKey(['key']);
