7a9f0829e00aa4207b5cc94e971eb3b177a02ec7
root <brent@manyou.io>
2023-10-04 04:47:25 +0000

    u

--- Bundle/MangoBundle.php
+++ Bundle/MangoBundle.php
@@ -5,71 +5,26 @@ declare(strict_types=1);
 namespace Mango\Bundle;
 
 use Doctrine\Migrations\Provider\SchemaProvider as SchemaProviderInterface;
-use LogicException;
-use Mango\DependencyInjection\DoctrineConnectionPass;
 use Mango\DependencyInjection\DoctrineEnableSavepointPass;
 use Mango\DependencyInjection\DoctrineMigrationsDependencyPass;
 use Mango\DependencyInjection\DoctrineTypePass;
 use Mango\DependencyInjection\HttpKernelControllerPass;
-use Mango\DependencyInjection\MessengerMiddlewarePass;
-use Mango\DependencyInjection\MonologChannelPass;
 use Mango\Doctrine\SchemaProvider;
-use Mango\Doctrine\Type\LogLevelType;
 use Mango\Doctrine\Type\UlidType;
 use Mango\Doctrine\Type\UsDateTimeImmutableType;
 use Mango\Doctrine\Type\UuidType;
-use Mango\HttpKernel\AsPayloadInitializer;
-use Mango\Scheduler\Messenger\RecurringScheduleMiddleware;
-use Mango\TaskQueue\Doctrine\Type\TaskStatusType;
-use Mango\TaskQueue\Messenger\Middleware\TaskQueueMiddware;
-use ReflectionClass;
-use ReflectionMethod;
-use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
-
-use function get_object_vars;
-use function sprintf;
 
 class MangoBundle extends AbstractBundle
 {
     public function build(ContainerBuilder $container): void
     {
-        $container->registerAttributeForAutoconfiguration(
-            AsPayloadInitializer::class,
-            static function (ChildDefinition $definition, AsPayloadInitializer $attribute, ReflectionClass|ReflectionMethod $reflector): void {
-                $tagAttributes = get_object_vars($attribute);
-                if ($reflector instanceof ReflectionMethod) {
-                    if (isset($tagAttributes['method'])) {
-                        throw new LogicException(
-                            sprintf('AsDtoInitializer attribute cannot declare a method on "%s::%s()".', $reflector->class, $reflector->name),
-                        );
-                    }
-
-                    $tagAttributes['method'] = $reflector->getName();
-                }
-
-                $definition->addTag('mango.request_payload_initializer', $tagAttributes);
-            },
-        );
-
         $container->addCompilerPass(new HttpKernelControllerPass());
 
         $container->addCompilerPass(
-            new MessengerMiddlewarePass(['id' => TaskQueueMiddware::class]),
-            priority: 1,
-        );
-
-        $container->addCompilerPass(
-            new MessengerMiddlewarePass(['id' => RecurringScheduleMiddleware::class]),
-            priority: 2,
-        );
-
-        $container->addCompilerPass(
             new DoctrineTypePass([
-                TaskStatusType::NAME => TaskStatusType::class,
-                LogLevelType::NAME => LogLevelType::class,
                 'ulid' => UlidType::class,
                 'uuid' => UuidType::class,
                 UsDateTimeImmutableType::NAME => UsDateTimeImmutableType::class,
@@ -79,19 +34,6 @@ class MangoBundle extends AbstractBundle
 
         $container->addCompilerPass(
             new DoctrineMigrationsDependencyPass([SchemaProviderInterface::class => SchemaProvider::class]),
-        );
-
-        $container->addCompilerPass(
-            new MonologChannelPass(
-                ['task'],
-                ['monolog.handler.task_queue' => 'task'],
-            ),
-            priority: 1,
-        );
-
-        $container->addCompilerPass(
-            new DoctrineConnectionPass(['logging' => 'doctrine.dbal.logging_connection']),
-            priority: 1,
         );
 
         $container->addCompilerPass(new DoctrineEnableSavepointPass());
--- Bundle/Resources/config/services.php
+++ Bundle/Resources/config/services.php
@@ -4,19 +4,6 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Doctrine\DBAL\Connection;
-use Mango\ApiPlatform\SerializerInitializerContextBuilder;
-use Mango\Doctrine\Driver\Oci8InitializeSession;
-use Mango\Doctrine\SchemaProvider;
-use Mango\Serializer\MoneyNormalizer;
-use Mango\TaskQueue\Doctrine\Table\TaskLogsTable;
-use Mango\TaskQueue\Doctrine\Table\TasksTable;
-use Mango\TaskQueue\Messenger\Middleware\TaskQueueMiddware;
-use Mango\TaskQueue\Monolog\TaskLogHandler;
-use Monolog\Handler\FingersCrossedHandler;
-use Monolog\Level;
-use Monolog\Processor\PsrLogMessageProcessor;
-
 use function dirname;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -29,46 +16,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->load('Mango\\', dirname(__DIR__, 3) . '/')
         ->exclude(dirname(__DIR__, 3) . '/{Bundle,Tests}');
-
-    $services->set(SchemaProvider::class)->public();
-    $services->set(Oci8InitializeSession::class);
-    $services->set(TasksTable::class);
-    $services->set(TaskLogsTable::class);
-    $services->set(TaskQueueMiddware::class);
-
-    $services->set('mango.monolog.processor.psr', PsrLogMessageProcessor::class)
-        ->arg('$dateFormat', 'Y-m-d\TH:i:s.vp')
-        ->arg('$removeUsedContextFields', true);
-
-    $services->set('doctrine.dbal.logging_connection.configuration')
-        ->parent('doctrine.dbal.connection.configuration')
-        ->call('setSchemaManagerFactory', [service('doctrine.dbal.default_schema_manager_factory')]);
-
-    $services->set('doctrine.dbal.logging_connection.event_manager')
-        ->parent('doctrine.dbal.connection.event_manager');
-
-    $services->set('doctrine.dbal.logging_connection')
-        ->parent('doctrine.dbal.connection')
-        ->public()
-        ->args([
-            ['url' => env('DATABASE_URL')->resolve()],
-            service('doctrine.dbal.logging_connection.configuration'),
-            service('doctrine.dbal.logging_connection.event_manager'),
-        ]);
-
-    $services->set('mango.doctrine.schema_provider.logging')
-        ->class(SchemaProvider::class)
-        ->arg(Connection::class, service('doctrine.dbal.logging_connection'));
-
-    $services->set(TaskLogHandler::class)
-        ->args([service('mango.doctrine.schema_provider.logging'), Level::Debug->value, false])
-        ->call('setFormatter', [service('monolog.formatter.normalizer')]);
-
-    $services->set('monolog.handler.task_queue', FingersCrossedHandler::class)
-        ->args([service(TaskLogHandler::class), Level::Info->value, 30, true])
-        ->call('pushProcessor', [service('mango.monolog.processor.psr')]);
-
-    $services->set(SerializerInitializerContextBuilder::class);
-
-    $services->set(MoneyNormalizer::class);
 };
