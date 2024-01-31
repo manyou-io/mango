ae0ff423d742c59ab47185e7276a2ff36f7046e7
root <brent@manyou.io>
2024-01-31 03:18:39 +0000

    Add PgArrayType

---
+++ Doctrine/Type/PgArrayType.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mango\Doctrine\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use JsonException;
+
+class PgArrayType extends Type
+{
+    public const NAME = 'pg_array';
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    {
+        return 'text[]';
+    }
+
+    public function convertToPHPValueSQL($sqlExpr, $platform): string
+    {
+        return $sqlExpr;
+    }
+
+    public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform): string
+    {
+        return $sqlExpr;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param T $value
+     *
+     * @return (T is null ? null : string)
+     *
+     * @template T
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        try {
+            return json_encode($value, JSON_THROW_ON_ERROR | JSON_PRESERVE_ZERO_FRACTION);
+        } catch (JsonException $e) {
+            throw ConversionException::conversionFailedSerialization($value, 'json', $e->getMessage(), $e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        if (is_resource($value)) {
+            $value = stream_get_contents($value);
+        }
+
+        try {
+            return json_decode($value, true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            throw ConversionException::conversionFailed($value, $this->getName(), $e);
+        }
+    }
+}
