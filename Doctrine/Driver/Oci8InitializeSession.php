9cece208780d30ad6dd655a202b78ba618ee0ab6
root <brent@manyou.io>
2024-03-22 07:36:24 +0000

    Update dependency versions & tests

--- Doctrine/Driver/Oci8InitializeSession.php
+++ Doctrine/Driver/Oci8InitializeSession.php
@@ -5,15 +5,23 @@ declare(strict_types=1);
 namespace Mango\Doctrine\Driver;
 
 use Doctrine\DBAL\Driver;
-use Doctrine\DBAL\Driver\OCI8\Driver as OCI8Driver;
+use Doctrine\DBAL\Driver\Middleware;
+use Doctrine\DBAL\Driver\OCI8;
 use Doctrine\DBAL\Driver\OCI8\Middleware\InitializeSession;
 
-class Oci8InitializeSession extends InitializeSession
+class Oci8InitializeSession implements Middleware
 {
+    private readonly Middleware $decorated;
+
+    public function __construct()
+    {
+        $this->decorated = new InitializeSession();
+    }
+
     public function wrap(Driver $driver): Driver
     {
-        if ($driver instanceof OCI8Driver) {
-            return parent::wrap($driver);
+        if ($driver instanceof OCI8\Driver) {
+            return $this->decorated->wrap($driver);
         }
 
         return $driver;
